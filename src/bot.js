d555303403d073ec15d5fec6faff05372e885bd8
bbaxa16 <brooke769@gmail.com>
2018-03-01 20:46:47 -0700

    perfection

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,22 @@
 const Twit = require('twit');
-const congig = require('./config');
+const config = require('./config');
 
 const bot = new Twit(config);
-function resign(){}
+
+let d = new Date();
+let n = d.getHours();
+let m = d.getMinutes();
+
+function resign() {
   bot.post('statuses/update', {
-    status:
-    '@RebLebsock RESIGN'
-  }, (err, data, response) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('${data.text} tweeted!');
-    }
+  status: 'It is ' + n + ':' + m + '. Time for @RepLebsock to resign #timesup'
+}, (err, data, response) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(`${data.text} tweeted!`)
   }
-setInterval(resign(), 1000*60*60)
 })
+}
+resign();
+setInterval(resign, 1000*60*60)
